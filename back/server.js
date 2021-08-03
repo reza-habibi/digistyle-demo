3443c88c99f56ff512557b7d9434aefa18ef4f63
reza-habibi <rezhabibi72@gmail.com>
2021-08-03 13:38:04 +0430

    get products from server

--- back/server.js
+++ back/server.js
@@ -1,17 +1,27 @@
-import express from 'express'
-import {data} from './data.js'
+import express from "express";
+import { data } from "./data.js";
 
 const app = express();
-app.get('/',(rea,res)=>{
-  res.send('Server is ready');
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
 
-app.get('/api/products' , (req , res) => {
-  res.send(data.products)
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "محصول مورد نظر شما یافت نشد" });
+  }
+});
 
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 
-app.listen(port , ()=>{
-  console.log(`server at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`server at http://localhost:${port}`);
+});
