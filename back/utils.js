c200a7d9ce541fb1a035eb91609b805b1ba2c7bb
Reza <64686682+reza-habibi@users.noreply.github.com>
2021-08-30 09:47:16 +0430

    Update utils.js

--- back/utils.js
+++ back/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (user) => {
       email: user.email,
       isAdmin: user.isAdmin,
     },
-    process.env.JWT_SECRET || "kosenanelongia",
+    process.env.JWT_SECRET || "gorebabalong",
     {
       expiresIn: "30d",
     }
@@ -21,7 +21,7 @@ export const isAuth = (req, res, next) => {
     const token = authorization.slice(7, authorization.length);
     jwt.verify(
       token,
-      process.env.JWT_SECRET || "kosenanelongia",
+      process.env.JWT_SECRET || "gorebabalong",
       (err, decode) => {
         if (err) {
           res.status(401).send({ message: "Invalid token" });
