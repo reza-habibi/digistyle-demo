6d3c09876985f8b83e842e3a953e6e35596af3f5
reza-habibi <rezhabibi72@gmail.com>
2021-10-19 08:53:53 +0330

    can i make it online ?

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,6 @@ import React, { lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Categories } from "./data";
 import { routes, PVRoute } from "./Routes/Routes";
-import { ToastContainer } from "react-toastify";
 import CustomRoute from "./Routes/CustomRoute";
 
 const Header = lazy(() => import("./components/Header/Header"));
@@ -15,7 +14,6 @@ const SubCategory = lazy(() => import("./screens/SubCategoryScreen"));
 function App() {
   return (
     <Router>
-      <ToastContainer />
       <Header />
       <MobileHeader />
 
