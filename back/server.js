72bb7ca74bb938fcbdf32c06b4434419cc13e813
Reza <64686682+reza-habibi@users.noreply.github.com>
2021-10-27 16:51:08 +0330

    Update server.js

--- back/server.js
+++ back/server.js
@@ -13,8 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://digisyle-admin:BigReZ72@digistyle.pvprh.mongodb.net/digistyle?retryWrites=true&w=majority",
+  process.env.MONGODB_URI ,
   {
     useNewUrlParser: true,
     useFindAndModify: true,
